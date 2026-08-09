ed04d105454a911aea5c4291b54a01eadb8476b7
Yann Le Doaré <yann@linuxconsole.org>
2026-08-09 23:40:53 +0200

    Update desktop-install-menus.sh

--- 2.12/desktop-install-menus/desktop-install-menus.sh
+++ 2.12/desktop-install-menus/desktop-install-menus.sh
@@ -1,12 +1,21 @@
-# rm /usr/bin/glade
 for file in usr/share/desktop-directories/Install.directory usr/share/applications/install-firefox.desktop usr/share/pixmaps/firefox.png \
-	usr/share/applications/steam.desktop usr/share/pixmaps/steam.png bin/steam
+	usr/share/applications/install-flatpak.desktop usr/share/pixmaps/flatpak.png
 do
   dirname=$( dirname $file )
   basename=$( basename $file )
   cd /
   install -d $dirname
-  # https://bitbucket.org/yourdistrofromscratch/ydfs/raw/master/2.8/
-  wget -q https://opkg.linuxconsole.org/linuxconsole/2.10/$file
+  wget -q https://opkg.linuxconsole.org/linuxconsole/2.12/$file
   mv $basename $dirname
+done
+
+for file in usr/bin/install-flatpak
+do
+  dirname=$( dirname $file )
+  basename=$( basename $file )
+  cd /
+  install -d $dirname
+  wget -q https://opkg.linuxconsole.org/linuxconsole/2.12/$file
+  mv $basename $dirname
+  chmod +x $dirname/$basename
 done
